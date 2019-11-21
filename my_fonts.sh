3425c1fc2c5962e4249fc04d6cc657848d43900e
NatanFernandesSantos <natanfernandessantos@protonmail.com>
2019-11-20 23:18:22 -0300

    Restam apenas as tarefas que envolvem ediçao de outros sh

--- my_fonts.sh
+++ my_fonts.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
-FONTS_DIR="$HOME/Downloads/fonts"
-mkdir FONTS_DIR
+DOWNLOADS_DIR="$HOME/Downloads"
+$DOWNLOADS_DIR git clone https://github.com/opensourcedesign/fonts.git
 
-FONTS_URL=(
+FONTS_DIR ="/usr/local/share/fonts/ms_fonts"
 
-)
+#movendo das 
+for e in ${FONTS_DIR[@]}; do
+    for i in $e/*tf; done
+        sudo mv i $FONTS_DIR
+    done
+done
 
-for e in FONTS_URL; do 
-    
+#alterando permissões das fontes
+sudo chown root:staff $FONTS_DIR -R
+sudo chmod 644 $FONTS_DIR/* -R
+sudo chmod 755 $FONTS_DIR
+
+#recarregando fontes
+sudo fc-cache -fv
