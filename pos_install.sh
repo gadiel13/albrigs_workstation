d61bbf7ac92bb9183ad33ae0fff175744269c383
Natan Fernandes <41878087+NatanFernandesSantos@users.noreply.github.com>
2020-02-17 00:33:52 -0300

    Update pos_install.sh

--- pos_install.sh
+++ pos_install.sh
@@ -181,6 +181,7 @@ done
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y flathub com.github.libresprite.LibreSprite
+flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
 
 #instalando os .deb
 for e in ./*.deb; do
